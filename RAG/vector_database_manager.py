bdf724491a625fabf6f3b4acbdf76a579b222ec8
Eric-LLMs <eric.zhao.engineer@gmail.com>
2024-09-10 19:21:22 +0800

    add RAG pipline

--- RAG/vector_database_manager.py
+++ RAG/vector_database_manager.py
@@ -17,70 +17,73 @@ _ = load_dotenv(find_dotenv())
 
 class MyVectorDBConnector:
     def __init__(self, collection_name, embedding_fn):
-        """向量数据库操作对象"""
+        """Vector database operations object"""
 
-        chroma_client = chromadb.HttpClient(host="localhost", port='8000')  # docker客户端模式
+        chroma_client = chromadb.HttpClient(host="localhost", port='8000')  # Docker client mode
 
-        # chroma_client = chromadb.Client(Settings(allow_reset=True)) #内存模式
-        # # 为了演示，实际不需要每次 reset()
+        # chroma_client = chromadb.Client(Settings(allow_reset=True))  # In-memory mode
+        # # For demonstration purposes; in practice, there's no need to reset each time
         # chroma_client.reset()
 
         # chroma_client = chromadb.Client(Settings(
-        #     allow_reset=False,  # 确保不会每次启动时重置数据
-        #     chroma_db_impl="sqlite",  # 使用 SQLite 作为数据库实现
-        #     persist_directory="./data/chroma_data"  # 数据存储路径
+        #     allow_reset=False,  # Ensure data is not reset on each startup
+        #     chroma_db_impl="sqlite",  # Use SQLite as the database implementation
+        #     persist_directory="./data/chroma_data"  # Path for data storage
         # ))
 
-        # chroma_client = chromadb.PersistentClient(path="./data/chroma_data")  # 数据保存在磁盘 sqlite
+        # chroma_client = chromadb.PersistentClient(path="./data/chroma_data")  # Data is stored on disk using SQLite
 
         # Create or get a collection
         self.collection = chroma_client.get_or_create_collection(
             name=collection_name
         )
 
-        # 创建一个 collection
+        # Create a collection
         self.collection = chroma_client.get_or_create_collection(
             name=collection_name)
         self.embedding_fn = embedding_fn
 
     def add_documents(self, documents, ids):
-        '''向 collection 中添加文档与向量'''
+        '''Add documents and vectors to the collection'''
         self.collection.add(
-            embeddings=self.embedding_fn(documents),  # 每个文档的向量
-            documents=documents,  # 文档的原文
-            # ids=[f"id{i}" for i in range(len(documents))]  # 每个文档的 id
+            embeddings=self.embedding_fn(documents),  # Vectors for each document
+            documents=documents,  # Original content of the documents
+            # ids=[f"id{i}" for i in range(len(documents))]  # ID for each document
             ids=ids
         )
 
-        # 如果 Chroma 收到一个文档列表，它会自动标记并使用集合的嵌入函数嵌入这些文档（如果在创建集合时没有提供嵌入函数，则使用默认值）。Chroma也会存储文档本身。如果文档过大，无法使用所选的嵌入函数嵌入，则会出现异常。
-        # 每个文档必须有一个唯一的相关ID。尝试.添加相同的ID两次将导致错误。可以为每个文档提供一个可选的元数据字典列表，以存储附加信息并进行过滤。
-        # 或者，您也可以直接提供文档相关嵌入的列表，Chroma将存储相关文档，而不会自行嵌入。
+        # If Chroma receives a list of documents, it will automatically tag and embed these documents using the collection's embedding function
+        # (if no embedding function was provided when creating the collection, the default is used). Chroma will also store the documents themselves.
+        # If a document is too large to be embedded using the selected embedding function, an exception will occur.
+        # Each document must have a unique associated ID. Attempting to add the same ID twice will result in an error.
+        # An optional list of metadata dictionaries can be provided for each document to store additional information and enable filtering.
+        # Alternatively, you can directly provide a list of document-related embeddings, and Chroma will store the associated documents without embedding them itself.
         # collection.add(
-        #     embeddings=[[1.2, 2.3, 4.5], [6.7, 8.2, 9.2]],
+        #     embeddings=[[0.1378, ..., 0.235], [0.0217, ..., 0.0032]],
         #     documents=["This is a document", "This is another document"],
         #     metadatas=[{"source": "my_source"}, {"source": "my_source"}],
         #     ids=["id1", "id2"]
         # )
-        # 触发重新计算 ANN 索引（如果支持的话）
+        # Trigger re-calculation of the ANN index (if supported)
         # self._rebuild_index_if_needed()
 
     # def _rebuild_index_if_needed(self):
-    #     # 根据需要手动触发重新计算索引
-    #     # 这仅是示例，具体实现取决于所使用的库
+    #     # Manually trigger index re-calculation if needed
+    #     # This is just an example; the specific implementation depends on the library used
     #     pass
 
     # def reset_collection(self):
-    #     self.chroma_client.reset()  # 可能需要根据具体实现
+    #     self.chroma_client.reset()  # May need adjustments based on the specific implementation
     #     self.collection = self.chroma_client.get_or_create_collection(name=self.collection_name)
 
-    def get_document_by_id(self, document_ids) -> tuple[list[str],list[str]]:
+    def get_document_by_id(self, document_ids) -> tuple[list[str], list[str]]:
         # results = self.collection.get(ids=[document_id])
         results = self.collection.get(ids=document_ids)
         # return results["documents"][0] if results["documents"] else None
         return results["ids"], results["documents"]
 
     def search(self, query, top_n):
-        '''检索向量数据库'''
+        '''Search the vector database'''
         results = self.collection.query(
             query_embeddings=self.embedding_fn([query]),
             n_results=top_n
@@ -88,25 +91,25 @@ class MyVectorDBConnector:
         return results
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
 
     client = OpenAI()
 
-    llmmager = LLMManager(client, 'text-embedding-ada-002')
+    llm_manager = LLMManager(client, 'text-embedding-ada-002')
 
     collection_name = "llama2_docs"
     # collection_name = "demo"
     # Create a vector database object and add documents
-    vector_db = MyVectorDBConnector(collection_name, llmmager.get_embeddings_default)
+    vector_db = MyVectorDBConnector(collection_name, llm_manager.get_embeddings_default)
 
     ids, docs = load_docs()
 
-    # 检查文档是否存在
+    # Check if documents exist
     existing_document = vector_db.get_document_by_id(ids)
-    # 没有生成向量的新文档
+    # Identify new documents that need vector generation
     new_ids = set(ids) - set(existing_document[0])
 
-    # 原序保留需要生成向量的文档列表
+    # Preserve order of documents needing vector generation
     add_ids = []
     add_docs = []
 
@@ -116,8 +119,8 @@ if __name__=="__main__":
             add_docs.append(doc)
 
     if len(add_ids) > 0:
-        # # 文档不存在，添加新文档
-        # # 加入一次就可以，向向量数据库中添加文档
+        # # Document does not exist, add new documents
+        # # Add only once, add documents to the vector database
         # # Extract text from the specified pages of the PDF
         # paragraphs = extract_text_from_pdf(
         #     "data/llama2.pdf",
@@ -126,24 +129,25 @@ if __name__=="__main__":
         # )
         # vector_db.add_documents(paragraphs)
         vector_db.add_documents(documents=add_docs, ids=add_ids)
-        print("文档已添加" + "\n")
+        print("Documents have been added" + "\n")
     else:
-        print("文档已存在，无需重复添加" + "\n")
+        print("Documents already exist, no need to add again" + "\n")
 
-    user_query = "Llama 2有多少参数"
+    user_query = "How many parameters does LLaMA 2 have?"
     results = vector_db.search(user_query, 2)
 
     # Print out the results
     for result in results['documents'][0]:
         print(result)
 
-    # the output is as following:
-    # In this work, we develop and release Llama 2, a family of pretrained and fine-tuned LLMs, Llama 2 and Llama
-    # 2-Chat, at scales up to 70B parameters. On the series of helpfulness and safety benchmarks we tested, Llama 2-Chat
-    # models generally perform better than existing open-source models. They also appear to be on par with some of the
-    # closed-source models, at least on the human evaluations we performed (see Figures 1 and 3).
-    # In this work, we develop and release Llama 2, a collection of pretrained and fine-tuned large language models
-    # (LLMs) ranging in scale from 7 billion to 70 billion parameters. Our fine-tuned LLMs, called Llama 2-Chat, are
-    # optimized for dialogue use cases. Our models outperform open-source chat models on most benchmarks we tested, and
-    # based on our human evaluations for helpfulness and safety, may be a suitable substitute for closedsource models.
+
+# the output is as following:
+# In this work, we develop and release Llama 2, a family of pretrained and fine-tuned LLMs, Llama 2 and Llama
+# 2-Chat, at scales up to 70B parameters. On the series of helpfulness and safety benchmarks we tested, Llama 2-Chat
+# models generally perform better than existing open-source models. They also appear to be on par with some of the
+# closed-source models, at least on the human evaluations we performed (see Figures 1 and 3).
+# In this work, we develop and release Llama 2, a collection of pretrained and fine-tuned large language models
+# (LLMs) ranging in scale from 7 billion to 70 billion parameters. Our fine-tuned LLMs, called Llama 2-Chat, are
+# optimized for dialogue use cases. Our models outperform open-source chat models on most benchmarks we tested, and
+# based on our human evaluations for helpfulness and safety, may be a suitable substitute for closedsource models.
 
